9da8874daa721c5e1f6dac5eaa462e2d8b57c6b3
MasonSchnell <91274207+MasonSchnell@users.noreply.github.com>
2023-11-09 22:22:41 -0500

    Added first draft of code into src-sw, webpack, and database.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,29 +1,70 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
-module.exports = () => {
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  const plugins = [
+    // 1
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      chunks: ["main"],
+      filename: "index.html",
+    }),
+    isProduction &&
+      new InjectManifest({
+        swSrc: "./src-sw.js", // Path to your service worker file
+        swDest: "src-sw.js",
+      }),
+    new WebpackPwaManifest({
+      name: "Text Editor",
+      short_name: "Text",
+      description: "Edit your text",
+      background_color: "#225ca3",
+      theme_color: "#225ca3",
+      icons: [
+        {
+          src: path.resolve("src/images/logo.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
+        },
+      ],
+    }),
+  ];
+
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
-    plugins: [
-      
-    ],
+    plugins: plugins,
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
       ],
     },
   };
